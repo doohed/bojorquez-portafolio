e2bb3aa8c0d8c01c104aabb71ce561347c4ce25e
doohed <dihed1973@gmail.com>
2024-04-28 21:27:38 -0700

    added animations on the landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,8 +21,16 @@ const Top = styled.div`
 `;
 
 const Message = styled.div`
-width: 25vw;
-user-select: none;
+  width: 25vw;
+  user-select: none;
+`;
+
+const Square = styled.div`
+  height: 90vh;
+  width: 50vw;
+  background-color: black;
+  z-index: 1;
+  overflow: hidden;
 `;
 
 const Hero = () => {
@@ -30,6 +38,31 @@ const Hero = () => {
     <Section>
       <Container>
         <Top>
+          <Square
+            className=" animate-[squareIn_2s_ease-in-out]
+          relative left-[50%] 
+          translate-x-[-50%] 
+          top-[50%] 
+          translate-y-[-50%]"
+          >
+            <div
+              className="
+          absolute left-[50%] 
+          translate-x-[-50%] 
+          top-[50%] 
+          translate-y-[-50%]"
+            >
+              <Title className="invert animate-[titleIn_.7s_ease-in-out]">
+                MARTIN BOJORQUEZ
+              </Title>
+              <Title className="invert animate-[titleIn_1s_ease-in-out]">
+                MARTIN BOJORQUEZ
+              </Title>
+              <Title className="invert animate-[titleIn_1.5s_ease-in-out]">
+                MARTIN BOJORQUEZ
+              </Title>
+            </div>
+          </Square>
           <div
             className="
           absolute left-[50%] 
@@ -37,20 +70,22 @@ const Hero = () => {
           top-[50%] 
           translate-y-[-50%]"
           >
-            <Title className="animate-[titleIn_.7s_ease-in-out]">
+            <Title className=" animate-[titleIn_.7s_ease-in-out]">
               MARTIN BOJORQUEZ
             </Title>
-            <Title className="animate-[titleIn_1s_ease-in-out] text-white">
+            <Title className="text-white animate-[titleIn_1s_ease-in-out]">
               MARTIN BOJORQUEZ
             </Title>
-            <Title className="animate-[titleIn_1.5s_ease-in-out]">
+            <Title className=" animate-[titleIn_1.5s_ease-in-out]">
               MARTIN BOJORQUEZ
             </Title>
           </div>
-          <Message className="absolute left-[50%] 
+          <Message
+            className="absolute left-[50%] 
           translate-x-[-50%] 
           bottom-[0vh]
-          ">
+          "
+          >
             <p className="animate-bounce"> SCROLL DOWN</p>
           </Message>
         </Top>
