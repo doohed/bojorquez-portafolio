aa5096c3c39472ac4add79d715f8a8217df9c12b
doohed <dihed1973@gmail.com>
2024-04-30 15:28:31 -0700

    made the footer smaller

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
   return (
     <div className="h-[5vh]">
       <a>
-        <h1 className="text-left text-2xl ml-[2.5vw] max-[960px]:text-1xl">
+        <h1 className="text-left text-2xl ml-[2.5vw] max-[960px]:text-xl">
           © 2024 BOJORQUEZ.DEV
         </h1>
       </a>
