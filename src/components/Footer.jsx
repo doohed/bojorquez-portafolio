37a0b27e09a9cfa369ddb0524a1b86c843ebe6da
doohed <dihed1973@gmail.com>
2024-05-01 04:17:11 -0700

    fixed dynamic render on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,7 @@
-import styled from "styled-components";
+
 
 const Footer = () => {
-  function hover() {
-    document.getElementById("cursor").style.backdropFilter = "invert(0)";
-    document.getElementById("cursor").style.border = "1px solid white";
-    document.getElementById("cursor").style.height = "50px";
-    document.getElementById("cursor").style.width = "50px";
-  }
 
-  function exitHover() {
-    document.getElementById("cursor").style.backdropFilter = "invert(1)";
-    document.getElementById("cursor").style.border = "0px solid white";
-    document.getElementById("cursor").style.height = "32px";
-    document.getElementById("cursor").style.width = "32px";
-  }
-
-  const Link = styled.a `
-    cursor: url(cursor.svg) 12 12, auto;
-  `;
 
   return (
     <div className="h-[5vh]">
